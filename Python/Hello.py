6f9acb2a43ac4232f9cdd08a5a51cfb7284278d4
Keljode <kellymbogning0@gmail.com>
2022-12-06 15:04:33 +0100

    Program en commentaire

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,3 +1,19 @@
-print("Hello Kelly")
-print("you are the best!")
-print("once again...!")
+# print("Hello Kelly")
+# print("you are the best!")
+# print("once again...!")
+
+# i = 1
+# while i<3:
+#     print("PoweHouse is the best")
+#     i = i+1
+
+# songs = ["Onana", "Don", "It's Over", "Hello"]
+# print("My favorite songs are: ")
+# i = 0
+# while i < len(songs):
+#     print(songs[i])
+#     i = i+1
+# print("What are yours?")
+# print("Thanks!")
+
+print("Jai misle programme en commentaire")
